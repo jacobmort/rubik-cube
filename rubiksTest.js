d20678a1bafc825babc022f919a31f544215684d
unknown <jacob@.(none)>
2012-05-17 21:24:33 -0700

    finish front inverse

--- rubiksTest.js
+++ rubiksTest.js
@@ -1,7 +1,10 @@
 RubikCubeTest = function(){
 
 }
-
+RubikCubeTest.prototype.runAll = function(){
+	return this.rotateFrontTest()
+	&& this.rotateFrontInvertTest();
+}
 RubikCubeTest.prototype.rotateFrontTest = function(){
 	cube = new RubikCube();
 	cube.f_face = [
@@ -66,6 +69,104 @@ RubikCubeTest.prototype.rotateFrontTest = function(){
     ];
     
     cube.rotateFront(false);
+    
+    return (cube.f_face[0][0] == f_solution[0][0])
+	&& (cube.f_face[0][1] == f_solution[0][1])
+    && (cube.f_face[0][2] == f_solution[0][2])
+    && (cube.f_face[1][0] == f_solution[1][0])
+	&& (cube.f_face[1][1] == f_solution[1][1])
+    && (cube.f_face[1][2] == f_solution[1][2])
+    && (cube.f_face[2][0] == f_solution[2][0])
+	&& (cube.f_face[2][1] == f_solution[2][1])
+    && (cube.f_face[2][2] == f_solution[2][2])
+    && (cube.b_face[0][1] == b_solution[0][1])
+    && (cube.b_face[0][2] == b_solution[0][2])
+    && (cube.b_face[1][0] == b_solution[1][0])
+	&& (cube.b_face[1][1] == b_solution[1][1])
+    && (cube.b_face[1][2] == b_solution[1][2])
+    && (cube.b_face[2][0] == b_solution[2][0])
+	&& (cube.b_face[2][1] == b_solution[2][1])
+    && (cube.b_face[2][2] == b_solution[2][2])
+    && (cube.r_face[0][1] == r_solution[0][1])
+    && (cube.r_face[0][2] == r_solution[0][2])
+    && (cube.r_face[1][0] == r_solution[1][0])
+	&& (cube.r_face[1][1] == r_solution[1][1])
+    && (cube.r_face[1][2] == r_solution[1][2])
+    && (cube.r_face[2][0] == r_solution[2][0])
+	&& (cube.r_face[2][1] == r_solution[2][1])
+    && (cube.r_face[2][2] == r_solution[2][2])
+	&& (cube.o_face[1][0] == o_solution[1][0])
+	&& (cube.o_face[1][1] == o_solution[1][1])
+    && (cube.o_face[1][2] == o_solution[1][2])
+    && (cube.o_face[2][0] == o_solution[2][0])
+	&& (cube.o_face[2][1] == o_solution[2][1])
+    && (cube.o_face[2][2] == o_solution[2][2])
+}
+
+RubikCubeTest.prototype.rotateFrontInvertTest = function(){
+	cube = new RubikCube();
+	cube.f_face = [
+		['f1','f2','f3'],
+		['f4','f5','f6'],
+		['f7','f8','f9']
+    ];
+    var f_solution = [
+		['f3','f6','f9'],
+		['f2','f5','f8'],
+		['f1','f4','f7']
+    ];
+    cube.b_face = [
+		['b1','b2','b3'],
+		['b4','b5','b6'],
+		['b7','b8','b9']
+    ];
+    var b_solution = [
+		['l3','l6','l9'],
+		['b4','b5','b6'],
+		['b7','b8','b9']
+    ];
+    cube.r_face = [
+		['r1','r2','r3'],
+		['r4','r5','r6'],
+		['r7','r8','r9']
+    ];
+    var r_solution = [
+		['b3','r2','r3'],
+		['b2','r5','r6'],
+		['b1','r8','r9']
+    ];
+    cube.l_face = [
+		['l1','l2','l3'],
+		['l4','l5','l6'],
+		['l7','l8','l9']
+    ];
+    var l_solution = [
+		['l1','l2','b7'],
+		['l4','l5','b8'],
+		['l7','l8','b9']
+    ];
+    cube.t_face = [
+		['t1','t2','t3'],
+		['t4','t5','t6'],
+		['t7','t8','t9']
+    ];
+    var t_solution = [
+		['t1','t2','t3'],
+		['t4','t5','t6'],
+		['r3','r6','r9']
+    ];
+	cube.o_face = [
+		['r1','r2','r3'],
+		['r4','r5','r6'],
+		['r7','r8','r9']
+    ];
+    var o_solution = [
+		['r1','r2','r3'],
+		['r4','r5','r6'],
+		['r7','r8','r9']
+    ];
+    
+    cube.rotateFront(true);
     
     return (cube.f_face[0][0] == f_solution[0][0])
 	&& (cube.f_face[0][1] == f_solution[0][1])
